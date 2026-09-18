d8456800e650d9d66e972fc1033b561c25e26377
lenigoertz <l.goertz4@166420.schulen-bn.de>
2026-09-18 09:27:43 +0200

    Update HalloRegister.java
    änderung

--- HalloRegister.java
+++ HalloRegister.java
@@ -1,6 +1,6 @@
 public class HalloRegister {
     public static void main(String[] args) {
-        System.out.println("Willkommen im Organspenderegister!");
+        System.out.print;("Willkommen im Kurs 2627")
         System.out.println("Diese Ausgabe kommt direkt vom Compiler.");
     }
 }
