80af8fdeca5ddd1e3ee8146bec97fa2fe9bf7c1f
SalemNassaj <s.nassaj@166420.schulen-bn.de>
2026-09-18 09:34:13 +0200

    Create Registerbegruessung.java
    neu

---
+++ Registerbegruessung.java
@@ -0,0 +1,6 @@
+public class Registerbegruessung {
+    public static void main(String[] args) {
+        System.out.println("Willkommen im Kurs 2627 EF IF");
+        System.out.println("test");
+    }
+}
