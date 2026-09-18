74e1be7dd2a8b609b5bfe8c6dd6c5eb8e1857675
Katharina787 <k.brechtel@166420.schulen-bn.de>
2026-09-18 09:37:40 +0200

    Create RegisterBegruessung.java
    neue Datei

---
+++ RegisterBegruessung.java
@@ -0,0 +1,9 @@
+public class RegisterBegruessung {
+
+    public static void main(String[] args){
+    System.out.printIn("Willkommen im Register");
+    System.out.printIn("Michael Patrick Kelly ist der beste");
+    }
+    }
+    
+    
