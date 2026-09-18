6ffa404026dd8fe41f34f951c081ab027868df85
SamanthaWer <s.werger@166420.schulen-bn.de>
2026-09-18 09:39:17 +0200

    update RegestrierBegruessung.java
    Halloddedcdxd

---
+++ RegestrierBegruessung.java
@@ -0,0 +1,8 @@
+public class RegestrierBegruessung {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+        System.out.println("Nein");
+        System.out.println("Keine Ahnung");
+        System.out.println("Tschüss");
+    }
+}
