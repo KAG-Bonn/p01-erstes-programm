01e261492d2515a842c517b6f57df06bcec8877d
Josefine1902 <j.kuehlwette@166420.schulen-bn.de>
2026-09-18 09:35:38 +0200

    Create RegisterBegruessung.java
    Neues File

---
+++ RegisterBegruessung.java
@@ -0,0 +1,6 @@
+public class RegisterBegruessung {
+    public static void main(string[] args) {
+        System.out.println("Wilkommen im Register");
+        System.out.println("Viel Spaß bei der Erkundung!");
+    }
+}
