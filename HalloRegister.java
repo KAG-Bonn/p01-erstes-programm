f7d83e78983849a409307382da01441234b2e52c
Josefine1902 <j.kuehlwette@166420.schulen-bn.de>
2026-09-18 09:28:37 +0200

    Update HalloRegister.java
    Wechsel der aussage

--- HalloRegister.java
+++ HalloRegister.java
@@ -1,6 +1,6 @@
 public class HalloRegister {
     public static void main(String[] args) {
-        System.out.println("Willkommen im Organspenderegister!");
+        System.out.println("Willkommen im Kurs 2627 EF IF!");
         System.out.println("Diese Ausgabe kommt direkt vom Compiler.");
     }
 }
