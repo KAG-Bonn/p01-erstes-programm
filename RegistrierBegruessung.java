36b29bd32867fbf091eb99fcdad32fe3c5e7ba1f
Nitrogenium0 <n.welter@166420.schulen-bn.de>
2026-09-18 09:38:09 +0200

    Create RegistrierBegruessung.java
    ja

---
+++ RegistrierBegruessung.java
@@ -0,0 +1,7 @@
+public class RegistrierBegruessung {
+    public static void main(String[] args) {
+        System.out.println("Willkommen im Organspende-Register!")
+        System.out.println("Diese Ausgabe ist toll!")
+        System.out.println("EEF")
+    }
+}
