7dfef951ecceae64b80afe5aaa7930b2e67bc015
elenadoro <e.hochguerte@166420.schulen-bn.de>
2026-09-18 09:28:24 +0200

    Update HalloRegister.java
    ausgabe geändert im main

--- HalloRegister.java
+++ HalloRegister.java
@@ -1,6 +1,6 @@
 public class HalloRegister {
     public static void main(String[] args) {
-        System.out.println("Willkommen im Organspenderegister!");
+        System.out.println("Willkommen im Kurs 26/27 EF");
         System.out.println("Diese Ausgabe kommt direkt vom Compiler.");
     }
 }
