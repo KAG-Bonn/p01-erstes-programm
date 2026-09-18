3db3d182342d48a562064d836044af9f1a32870d
Levi <l.liebner@166420.schulen-bn.de>
2026-09-18 09:35:58 +0200

    Create RegistrierBegruesunng.java
    Java Regsiter Neu

---
+++ RegistrierBegruesunng.java
@@ -0,0 +1,6 @@
+public class RegistrierBegruesunng {
+    public static void main(String[] args) {
+        System.out.println("Willkommen im Organspenderegister!");
+        System.out.println("Das Programm wurde von der Ef Programmiert ");
+    }
+}
