a22a92ff5c327661702dadd88cf30c4387d1256d
Mark Weber <m.weber@adenauer-bonn.de>
2026-09-18 09:33:15 +0200

    Create RegisterBegruessung.java
    weitere datei

---
+++ RegisterBegruessung.java
@@ -0,0 +1,8 @@
+public class RegisterBegruessung {
+    public static void main(String[] args) {
+        System.out.println("Willkommen im Organspende-Register");
+        System.out.println("Das Programm programmiert worden von dem Kurs 2627 EF IF");
+        System.out.println("---------------------------");
+        System.out.println("Das Organspende-Register ist eine digitale Schnittstelle für die Einwilligungen von Organspenden");
+    }
+}
